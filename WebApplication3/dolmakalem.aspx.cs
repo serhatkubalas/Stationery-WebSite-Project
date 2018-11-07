42c70baec73db1e3a69a5803bfafbeb4d300f654
serhatkubalas <44839947+serhatkubalas@users.noreply.github.com>
2018-11-07 17:24:50 +0300

    StationeryProject

---
+++ WebApplication3/dolmakalem.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class dolmakalem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source=SHYLQ\MSSQLSERVER1;Initial Catalog=dbKırtasiye;User ID=sa;Password=12345678");
+            baglanti.Open();
+            SqlDataAdapter dapt = new SqlDataAdapter("select * from viewdolmakalem", baglanti);
+
+            DataTable dt = new DataTable();
+            dapt.Fill(dt);
+
+            Repeaterdolma.DataSource = dt; //repeater'ın veri kaynağı dt'dir.
+            Repeaterdolma.DataBind(); // Repeater'ı alacağın bilgilerle doldur.
+            baglanti.Close();
+        }
+    }
+}
---
+++ WebApplication3/index.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source=SHYLQ\MSSQLSERVER1;Initial Catalog=dbKırtasiye;User ID=sa;Password=12345678");
+            baglanti.Open();
+            SqlDataAdapter dapt = new SqlDataAdapter("select * from viewBanners", baglanti);
+
+            DataTable dt = new DataTable();
+            dapt.Fill(dt);
+
+            Repeaterbanner.DataSource = dt; //repeater'ın veri kaynağı dt'dir.
+            Repeaterbanner.DataBind(); // Repeater'ı alacağın bilgilerle doldur.
+            baglanti.Close();
+            
+            
+        }
+
+        
+    }
+}
